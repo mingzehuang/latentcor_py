4ec38b0c304cf8ac54e14177510ef5189cf407e9
Mingze (Rico) Huang <mingzehuang@gmail.com>
2021-11-28 17:45:41 -0600

    Test unbias on approx method

--- latentcor/unbias_check.py
+++ latentcor/unbias_check.py
@@ -120,52 +120,74 @@ pyplot.plot(rhos, rhos, color = "r")
 pyplot.title("Ternary vs. Binary (approx)")
 pyplot.show()
 
-"""
-""""""Binary vs. Binary""""""
+"""Binary vs. Binary"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["bin", "bin"], rhos = rhos[r], XP = numpy.array([[.5, .5], [numpy.nan, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["bin", "bin"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["bin", "bin"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Binary vs. Binary")
+pyplot.title("Binary vs. Binary (original)")
 pyplot.show()
 
-""""""Truncated vs. Binary""""""
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Binary vs. Binary (approx)")
+pyplot.show()
+
+"""Truncated vs. Binary"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["tru", "bin"], rhos = rhos[r], XP = numpy.array([[.5, .5], [numpy.nan, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["tru", "bin"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["tru", "bin"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Truncated vs. Binary")
+pyplot.title("Truncated vs. Binary (original)")
 pyplot.show()
 
-""""""Continuous vs. Binary""""""
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Truncated vs. Binary (approx)")
+pyplot.show()
+
+"""Continuous vs. Binary"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["con", "bin"], rhos = rhos[r], XP = numpy.array([[numpy.nan, .5], [numpy.nan, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["con", "bin"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["con", "bin"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Continuous vs. Binary")
+pyplot.title("Continuous vs. Binary (original)")
 pyplot.show()
 
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Continuous vs. Binary (approx)")
+pyplot.show()
+
+
+"""
 """"""Need more check""""""
 """"""Ternary vs. Truncated""""""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
@@ -173,62 +195,89 @@ Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["ter", "tru"], rhos = rhos[r], XP = numpy.array([[.3, .5], [.5, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["ter", "tru"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["ter", "tru"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Ternary vs. Truncated")
+pyplot.title("Ternary vs. Truncated (original)")
 pyplot.show()
 
-""""""Binary vs. Truncated""""""
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Ternary vs. Truncated (approx)")
+pyplot.show()
+"""
+
+"""Binary vs. Truncated"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["bin", "tru"], rhos = rhos[r], XP = numpy.array([[.5, .5], [numpy.nan, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["bin", "tru"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["bin", "tru"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Binary vs. Truncated")
+pyplot.title("Binary vs. Truncated (original)")
 pyplot.show()
 
-""""""Truncated vs. Truncated""""""
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Binary vs. Truncated (approx)")
+pyplot.show()
+
+"""Truncated vs. Truncated"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["tru", "tru"], rhos = rhos[r], XP = numpy.array([[.5, .5], [numpy.nan, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["tru", "tru"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["tru", "tru"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Truncated vs. Truncated")
+pyplot.title("Truncated vs. Truncated (original)")
+pyplot.show()
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Truncated vs. Truncated (approx)")
 pyplot.show()
 
-""""""Continuous vs. Truncated""""""
+"""Continuous vs. Truncated"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["con", "tru"], rhos = rhos[r], XP = numpy.array([[numpy.nan, .5], [numpy.nan, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["con", "tru"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["con", "tru"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Continuous vs. Truncated")
+pyplot.title("Continuous vs. Truncated (original)")
+pyplot.show()
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Continuous vs. Truncated (approx)")
 pyplot.show()
 
-""""""Ternary vs. Ternary""""""
-rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
+"""Ternary vs. Ternary"""
+"""rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["ter", "ter"], rhos = rhos[r], XP = numpy.array([[.3, .5], [.3, .5]], dtype = float, ndmin = 2), showplot = False)[0]
@@ -240,25 +289,31 @@ data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[
 plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
 pyplot.plot(rhos, rhos, color = "r")
 pyplot.title("Ternary vs. Ternary")
-pyplot.show()
+pyplot.show()"""
 
-""""""Binary vs. Ternary""""""
+"""Binary vs. Ternary"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["bin", "ter"], rhos = rhos[r], XP = numpy.array([[.5, .3], [numpy.nan, .5]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["bin", "ter"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["bin", "ter"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Binary vs. Ternary")
+pyplot.title("Binary vs. Ternary (original)")
+pyplot.show()
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Binary vs. Ternary (approx)")
 pyplot.show()
 
-""""""Truncated vs. Ternary""""""
-rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
+"""Truncated vs. Ternary"""
+"""rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["tru", "ter"], rhos = rhos[r], XP = numpy.array([[.5, .3], [numpy.nan, .5]], dtype = float, ndmin = 2), showplot = False)[0]
@@ -270,32 +325,27 @@ data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[
 plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
 pyplot.plot(rhos, rhos, color = "r")
 pyplot.title("Truncated vs. Ternary")
-pyplot.show()
+pyplot.show()"""
 
-""""""Continuous vs. Ternary""""""
+"""Continuous vs. Ternary"""
 rhos = numpy.linspace(-1,1,100); rhorep = numpy.repeat(numpy.nan, len(rhos))
 Rrep = numpy.repeat(numpy.nan, len(rhos) * 3).reshape(len(rhos), 3)
 for r in range(len(rhos)):
     X = gen_data.gen_data(n = 100, tps = ["con", "ter"], rhos = rhos[r], XP = numpy.array([[numpy.nan, .3], [numpy.nan, .5]], dtype = float, ndmin = 2), showplot = False)[0]
     R_nc_org = latentcor.latentcor(X = X, tps = ["con", "ter"], method = "original", use_nearPD = False)[1]
-    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
+    R_nc_approx = latentcor.latentcor(X = X, tps = ["con", "ter"], method = "approx", use_nearPD = False)[1]
+    rhorep[r] = rhos[r]; Rrep[r, 0] = R_nc_org[1, 0]; Rrep[r, 1] = R_nc_approx[1, 0]; Rrep[r, 2] = numpy.corrcoef(X)[0,1]
 print(Rrep)
 print(rhorep)
-data = {"True latent correlation": rhorep, "Estimated latent correlation": Rrep[ : , 0]}
-plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation")
+data = {"True latent correlation": rhorep, "Estimated latent correlation (original)": Rrep[ : , 0]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (original)")
 pyplot.plot(rhos, rhos, color = "r")
-pyplot.title("Continuous vs. Ternary")
-pyplot.show()"""
+pyplot.title("Continuous vs. Ternary (original)")
+pyplot.show()
+data = {"True latent correlation": rhorep, "Estimated latent correlation (approx)": Rrep[ : , 1]}
+plot = seaborn.scatterplot(data = data, x = "True latent correlation", y = "Estimated latent correlation (approx)")
+pyplot.plot(rhos, rhos, color = "r")
+pyplot.title("Continuous vs. Ternary (approx)")
+pyplot.show()
 
-"""r = 0
-X = gen_data.gen_data(n = 100, tps = ["ter", "con"], rhos = rhos[r], XP = numpy.array([[.3, numpy.nan], [.5, numpy.nan]], dtype = float, ndmin = 2), showplot = False)[0]
-print(X)
-R_nc_org = latentcor.latentcor(X = X, tps = ["ter", "con"], method = "original", use_nearPD = False)[1]
-print(R_nc_org)
-rhorep[r] = rhos[r]
-print(rhorep)
-Rrep[r, 0] = R_nc_org[1, 0]
-print(Rrep)
-Rrep[r, 2] = numpy.corrcoef(X)[0,1]
-print(Rrep)"""
 
