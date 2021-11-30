bb13e10701044f5309fb0c06b01e3b330ae0644f
Mingze Huang <mingzehuang@gmail.com>
2021-11-30 01:06:54 -0600

    update

--- latentcor/data/ipol_22.sh
+++ latentcor/data/ipol_22.sh
@@ -2,13 +2,13 @@
 ##ENVIRONMENT SETTINGS; CHANGE WITH CAUTION
 #SBATCH --export=NONE                #Do not propagate environment
 #SBATCH --get-user-env=L             #Replicate login environment
-#SBATCH --partition=knl
+#SBATCH --partition=short
 
 ##NECESSARY JOB SPECIFICATIONS
 #SBATCH --job-name=ipol_22            #Set the job name to
-#SBATCH --time=23:00:00               #Set the wall clock limit to 6hr and 30min
+#SBATCH --time=02:00:00               #Set the wall clock limit to 6hr and 30min
 #SBATCH --nodes=1                    #Request 1 node
-#SBATCH --ntasks-per-node=72         #Request 8 tasks/cores per node
+#SBATCH --ntasks-per-node=48         #Request 8 tasks/cores per node
 #SBATCH --mem=80GB                     #Request 8GB per node
 #SBATCH --output=ipol_22.%j            #Send stdout/err to "Example2Out.[jobID]"
 
