1a073151cfa588a39bb7ccc99a5780d26208838c
Mingze (Rico) Huang <mingzehuang@gmail.com>
2021-12-24 15:20:32 -0600

    finish draft of documentation for latentcor.py

--- latentcor/latentcor.py
+++ latentcor/latentcor.py
@@ -18,31 +18,64 @@ print(len(numpy.sum(ampdata_mat==0, axis = 0)))
 print(ampdata_mat.shape[1])
 
 def latentcor(X, tps = None, method = "approx", use_nearPD = True, nu = 0.001, tol = 1e-8, ratio = 0.9, showplot = False):
-    """
+    """Estimate latent correlation for mixed types.
 
+    Estimation of latent correlation matrix from observed data of (possibly) mixed types (continuous/binary/truncated/ternary) based on the latent Gaussian copula model. Missing values (NA) are allowed. The estimation is based on pairwise complete observations.
+    
     Parameters
     ----------
-    X :
-        
-    tps :
-         (Default value = None)
-    method :
-         (Default value = "approx")
-    use_nearPD :
-         (Default value = True)
-    nu :
-         (Default value = 0.001)
-    tol :
-         (Default value = 1e-8)
-    ratio :
-         (Default value = 0.9)
-    showplot :
-         (Default value = False)
 
+    X : numpy.array or pandas.DataFrame
+        A numeric matrix or numeric data frame (n by p), where n is number of samples, and p is number of variables. Missing values (NA) are allowed, in which case the estimation is based on pairwise complete observations.
+    
+    tps : list or 1D-array
+          (Default value = None)
+          A vector of length p indicating the type of each of the p variables in `X`. Each element must be one of "con" (continuous), "bin" (binary), "ter" (ternary) or "tru" (truncated). If the vector has length 1, then all p variables are assumed to be of the same type that is supplied. The variable types are determined automatically using function `get_tps`. As automatic determination of variable types takes extra time, it is recommended to supply the types explicitly when they are known in advance.
+    
+    method : {'original', 'approx'}
+             (Default value = "approx")
+             The calculation method for latent correlations. Either "original" or "approx". If `method = "approx"`, multilinear approximation method is used, which is much faster than the original method, see Yoon et al. (2021) for timing comparisons for various variable types. If `method = "original"`, optimization of the bridge inverse function is used.
+    
+    use_nearPD : bool
+                 (Default value = True)
+                 `use.nearPD = True` gets nearest positive definite matrix for the estimated latent correlation matrix with shrinkage adjustment by `nu`. Output `R` is the same as `Rpointwise` if `use.nearPD = False`.
+    
+    nu : float
+         (Default value = 0.001)
+         Shrinkage parameter for the correlation matrix, must be between 0 and 1. Guarantees that the minimal eigenvalue of returned correlation matrix is greater or equal to `nu`. When `nu = 0`, no shrinkage is performed, the returned correlation matrix will be semi-positive definite but not necessarily strictly positive definite. When `nu = 1`, the identity matrix is returned (not recommended).
+    
+    tol : float
+          (Default value = 1e-8)
+          When `method = "original"`, specifies the desired accuracy of the bridge function inversion via uniroot optimization and is passed to `optimize`. When `method = "approx"`, this parameter is ignored.
+    
+    ratio : float
+            (Default value = 0.9)
+            When `method = "approx"`, specifies the boundary value for multilinear interpolation, must be between 0 and 1. When `ratio = 0`, no linear interpolation is performed (the slowest execution) which is equivalent to `method = "original"`. When `ratio = 1`, linear interpolation is always performed (the fastest execution) but may lead to high approximation errors. The default (recommended) value controls the approximation error and has fast execution, see Yoon et al. (2021) for details. When `method = "original"`, this parameter is ignored.
+    
+    showplot : bool
+               (Default value = False)
+               `showplot = True` generates a seaborn object `plot` with the heatmap of latent correlation matrix `R`. `plot = None` if `showplot = False`.
+    
     Returns
     -------
 
+    R : numpy.array
+        (p x p) Estimated latent correlation matrix for `X`.
+
+    Rpointwise : numpy.array
+                 (p x p) Point-wise estimates of latent correlations for `X`. This matrix is not guaranteed to be semi-positive definite. This is the original estimated latent correlation matrix without adjustment for positive-definiteness.
+
+    plot : {object, None}
+           Heatmap plot of latent correlation matrix `R`, None if `showplot = FALSE`.
+
+    K : numpy.array
+        (p x p) Kendall Tau (Tau-a) Matrix for `X`. 
+
+    zratios : numpy.array
+              A (2 x p) matrix corresponding to each variable. Returns numpy.na for continuous variable; proportion of zeros for binary/truncated variables; the cumulative proportions of zeros and ones (e.g. first value is proportion of zeros, second value is proportion of zeros and ones) for ternary variable.
+
     """
+    
     X = numpy.array(X, dtype = numpy.float32); nu = float(nu); tol = float(tol); ratio = float(ratio)
     """Check the supplied parameters are compatible with what is expected."""
     if (nu < 0) | (nu > 1):
