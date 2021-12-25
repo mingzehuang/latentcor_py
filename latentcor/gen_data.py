baf9c5478af81dbba901172133809a3c99531b5c
Mingze (Rico) Huang <mingzehuang@gmail.com>
2021-12-24 18:48:27 -0600

    Finish draft on documentation for gen_data.py

--- latentcor/gen_data.py
+++ latentcor/gen_data.py
@@ -10,25 +10,45 @@ def gen_data(n = 100, tps = ["ter", "con"], rhos = .5, copulas = "no", XP = None
 
     Parameters
     ----------
-    n :
-         (Default value = 100)
-    tps :
-         (Default value = ["ter")
-    "con"] :
+    n : int
+        (Default value = 100)
+        A positive integer indicating the sample size.
+
+    tps : {list, numpy.array}
+          (Default value = ["ter", "con"])
+          A vector indicating the type of each variable, could be `"con"` (continuous), `"bin"` (binary), `"tru"` (truncated) or `"ter"` (ternary). The number of variables is determined based on the length of types, that is `p = length(tps)`. The default value `["ter", "con"]` which creates two variables: the first one is ternary, the second one is continuous.
         
-    rhos :
-         (Default value = .5)
-    copulas :
-         (Default value = "no")
-    XP :
+    rhos : {list, numpy.array}
+           (Default value = .5)
+           A vector with lower-triangular elements of desired correlation matrix, e.g. `rhos = [.3, .5, .7]` means the correlation matrix is `[[1, .3, .5], [.3, 1, .7], [.5, .7, 1]]`. If only a scalar is supplied (`len(rhos) = 1`), then equi-correlation matrix is assumed with all pairwise correlations being equal to `rhos`. The default value is 0.5 which means correlations between any two variables are 0.5.
+
+    copulas : {list, numpy.array}
+              (Default value = "no")
+              A vector indicating the copula transformation f for each of the p variables, e.g. U = f(Z). Each element can take value `"no"` (f is identity), `"expo"` (exponential transformation) or `"cube"` (cubic transformation). If the vector has length 1, then the same transformation is applied to all p variables. The default value is `"no"`: no copula transformation for any of the variables.
+
+    XP : numpy.array
          (Default value = None)
-    showplot :
-         (Default value = False)
+         A (2 x p) matrix indicating proportion of zeros (for binary and truncated), and proportions of zeros and ones (for ternary) for each of the variables. For continuous variable, NA should be supplied. If `None`, the following values are automatically generated as elements of `XP` list for the corresponding data types:
+         For continuous variable, the corresponding value is [numpy.na, numpy.na];
+         for binary or truncated variable, the corresponding value is a number between 0 and 1 representing the proportion of zeros and numpy.na, the default value is [0.5, numpy.na];
+         for ternary variable, the corresponding value is a pair of numbers between 0 and 1, the first number indicates the the proportion of zeros, the second number indicates the proportion of ones. The sum of a pair of numbers should be between 0 and 1, the default value is [0.3, 0.5].
+
+    showplot : bool
+               (Default value = False)
+               If TRUE, generates the plot of the data when number of variables p is no more than 3.
 
     Returns
     -------
 
+    X : numpy.array
+        Generated data matrix (n by p) of observed variables.
+    
+    plotX : object
+            Visualization of the data matrix X.
+            Histogram if `p=1`. 2D Scatter plot if `p=2`. 3D scatter plot if `p=3`. Returns None if `showplot = False`.
+
     """
+    
     if (type(n) is not int) | (n <= 0):
         print("n should be a positive integer as sample size.")
         exit()
