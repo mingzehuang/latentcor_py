43f082b598e0ecd77e4c8bcdeffe8c5c2e647bc2
Mingze (Rico) Huang <mingzehuang@gmail.com>
2021-12-26 02:20:47 -0600

    Finalize setup.py

--- setup.py
+++ setup.py
@@ -30,8 +30,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
-    description="A package to estimate latent correlation",
+    description="Fast Computation of Latent Correlations for Mixed Data",
     install_requires=requirements,
     license="GNU General Public License v3",
     long_description=readme + '\n\n' + history,
