989028e618a1fc66c9789c92cf0eabe95202d927
Mingze (Rico) Huang <mingzehuang@gmail.com>
2021-12-26 02:51:51 -0600

    Specify dependency versions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', encoding="utf8") as readme_file:
 with open('HISTORY.rst', encoding="utf8") as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'scipy', 'statsmodels', 'seaborn', 'matplotlib', 'os', 'lzma', 'pickle', 'joblib']
+requirements = ['numpy>=1.21', 'scipy>=1.7', 'statsmodels>=0.13', 'seaborn>=0.11', 'matplotlib>=3.5', 'joblib>=1.1']
 
 setup_requirements = ['pytest-runner', ]
 
@@ -19,16 +19,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Mingze Huang, Christian L. Müller, Irina Gaynanova",
     author_email='mingzehuang@gmail.com, christian.mueller@stat.uni-muenchen.de, irinag@stat.tamu.edu',
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10'
     ],
@@ -45,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mingzehuang/latentcor_py',
-    version='1.0.0',
+    version='0.1.0',
     zip_safe=False,
 )
