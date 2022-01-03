eb6710496fd971fca8f474fc4844dba4e27c887c
Mingze (Rico) Huang <mingzehuang@gmail.com>
2022-01-03 14:38:50 -0600

    try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@ import sys
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../latentcor/'))
+sys.path.insert(0, os.path.abspath('../../latentcor/data'))
 
 
 
