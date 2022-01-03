95c07d5bb7620e9cea3efb83d547aaee0292e6ec
Mingze (Rico) Huang <mingzehuang@gmail.com>
2022-01-03 10:17:17 -0600

    So much time spent on specifying correct path, but finally it works!

--- latentcor/latentcor.py
+++ latentcor/latentcor.py
@@ -13,23 +13,23 @@ import pickle
 import lzma
 
 
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_10.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_10.xz"), "rb") as f:
     ipol_10 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_11.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_11.xz"), "rb") as f:
     ipol_11 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_20.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_20.xz"), "rb") as f:
     ipol_20 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_21.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_21.xz"), "rb") as f:
     ipol_21 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_22.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_22.xz"), "rb") as f:
     ipol_22 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_30.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_30.xz"), "rb") as f:
     ipol_30 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_31.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_31.xz"), "rb") as f:
     ipol_31 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_32.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_32.xz"), "rb") as f:
     ipol_32 = pickle.load(f)
-with lzma.open(os.path.join(os.path.abspath('../latentcor'), "data", "ipol_33.xz"), "rb") as f:
+with lzma.open(os.path.join("../latentcor", "data", "ipol_33.xz"), "rb") as f:
     ipol_33 = pickle.load(f)
 
 """
