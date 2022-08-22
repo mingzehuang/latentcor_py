4d041f0bf506a312daeddd8d49747e95c569d602
Mingze (Rico) Huang <mingzehuang@gmail.com>
2022-08-22 18:35:28 -0500

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages(include=['latentcor', 'latentcor.*']),
     test_suite='tests',
     url='https://github.com/mingzehuang/latentcor_py',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )
